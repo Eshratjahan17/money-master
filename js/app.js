fff973ee73a149d1cbcf1959be1891de932b523b
Eshratjahan17 <eshratjahan701@gmail.com>
2022-02-17 19:56:12 +0600

    saving portion done

--- js/app.js
+++ js/app.js
@@ -12,21 +12,41 @@ document.getElementById('calculate').addEventListener('click', function() {
     let cloth = document.getElementById('cloth-field').value;
     let clothNmuber = parseFloat(cloth);
     console.log(clothNmuber);
-
+    //total count
     let totalCost = foodNmuber + rentNmuber + clothNmuber;
-    console.log(totalCost);
+
+
     const expenseField = document.getElementById('total-expenses');
     expenseField.innerText = totalCost;
-
+    //balance count
     let balance = incomeNmuber - totalCost;
 
     const balanceField = document.getElementById('balance');
     balanceField.innerText = balance;
 
 
+});
+
+function savings() {
+    // let saveInput = document.getElementById('save-field').value;
+    // let saveAmountNumber = parseFloat(saveInput);
+    // console.log(saveAmountNumber);
+
+    let saveInput = document.getElementById('save-field').value;
+    let saveAmountNumber = parseFloat(saveInput);
+    console.log(saveAmountNumber);
+    let income = document.getElementById('income-field').value;
+    let incomeNmuber = parseFloat(income);
+    console.log(incomeNmuber);
+
+    let save = incomeNmuber * saveAmountNumber / 100;
+    console.log(save);
+
+    return save;
 
 
-
-
+}
+document.getElementById('save').addEventListener('click', function() {
+    savings();
 
 })
